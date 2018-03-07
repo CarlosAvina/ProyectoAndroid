f04b5aa4e8d8fde1184d339c6c5cdcd82ead87c7
Carlos Aviña <carlosavilu11@gmail.com>
2018-03-07 01:06:59 -0600

    Segundo commit

--- ProyectoAndroid/MainActivity.cs
+++ ProyectoAndroid/MainActivity.cs
@@ -7,12 +7,22 @@ namespace ProyectoAndroid
     [Activity(Label = "ProyectoAndroid", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        EditText _txtUsername;
+        EditText _txtPassword;
+        Button _btnSingIn;
+        Button _btnSingUp;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            _txtUsername = FindViewById<EditText>(Resource.Id.txtUser);
+            _txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+            _btnSingIn = FindViewById<Button>(Resource.Id.btnSignIn);
+            //_btnSingUp = FindViewById<Button>(Resource.Id.);
         }
     }
 }
--- ProyectoAndroid/Resources/Resource.designer.cs
+++ ProyectoAndroid/Resources/Resource.designer.cs
@@ -41,6 +41,31 @@ namespace ProyectoAndroid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f040002
+			public const int btnSignIn = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int btnSingUp = 2130968579;
+			
+			// aapt resource value: 0x7f040001
+			public const int txtPassword = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int txtUser = 2130968576;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
